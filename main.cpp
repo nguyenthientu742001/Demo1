0e321337c54b2f1237a75a4974f5208a4a3d4314
nguyenthientu742001 <nguyenthientu12345@gmail.com>
2024-12-17 16:52:32 +0700

    WordGameProject

--- main.cpp
+++ main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+string reading_file(string filename) {
+
+    ifstream instance;
+    instance.open(filename);
+
+    string line;
+    string para;
+    while (getline(instance, line)) {
+
+        para = para + ' ' + line;;
+
+    }
+
+    instance.close();
+
+    return para;
+}
+
+void choose_game(int userChoice, string easyWord, string mediumWord,string hardWord) {
+
+    switch (userChoice) {
+        case 1:
+            cout << easyWord;
+        case 2:
+            cout << mediumWord;
+        case 3:
+            cout << hardWord;
+
+    }
+}
+
 int main() {
-    cout << "Word Game" << endl;
+    string userName;
+    int userChoice;
+    string easyWord;
+    string mediumWord;
+    string hardWord;
+
+    cout << "Welcome to Wordle" << endl;
+    cout << "Please enter user name:";
+    getline(cin, userName);
+
+
+    cout << "1. Easy \n2. Medium \n3. Hard\n";
+    cout << "Please enter the level:";
+    cin >> userChoice;
+
+    easyWord = reading_file(R"(C:\Users\NGUYEN THIEN TU\Desktop\Game\WordGame\easy.txt)");
+    mediumWord = reading_file(R"(C:\Users\NGUYEN THIEN TU\Desktop\Game\WordGame\medium.txt)");
+    hardWord =  reading_file(R"(C:\Users\NGUYEN THIEN TU\Desktop\Game\WordGame\hard.txt)");
+
+
+    choose_game(userChoice, easyWord, mediumWord,hardWord);
+
+
 
     return 0;
 }
